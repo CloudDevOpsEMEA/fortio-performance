689c143778753b656c906461873ff2f4720567cf
Bart Van Bos <bartvanbos@gmail.com>
2021-03-27 22:34:10 +0100

    added wrapper to run all testscenarios

---
+++ scenarios/run_all_find_concurreny_max_qps.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+SCENARIO_ARRAY=(
+  "1pod-diffnode-nosidecar-1core"
+  "1pod-diffnode-nosidecar-2core"
+  "1pod-diffnode-nosidecar-3core"
+  "1pod-diffnode-nosidecar-maxcore"
+  "1pod-samenode-nosidecar-1core"
+  "1pod-samenode-nosidecar-2core"
+  "1pod-samenode-nosidecar-3core"
+  "1pod-samenode-nosidecar-maxcore"
+  "2pod-diffnode-nosidecar-1core"
+  "2pod-diffnode-nosidecar-maxcore"
+  "2pod-samenode-nosidecar-1core"
+  "3pod-diffnode-nosidecar-1core"
+  "3pod-diffnode-nosidecar-maxcore"
+  "3pod-samenode-nosidecar-1core"
+)
+
+for SCENARIO in "${SCENARIO_ARRAY[@]}" ; do
+
+  make uninstall-client uninstall-server delete-namespace 
+  sleep 20
+  make create-namespace install-client install-server SCENARIO=${SCENARIO}
+  sleep 20
+  make wait-ready
+
+  ./find_concurreny_max_qps.sh -l ${SCENARIO} -t 30s
+
+done

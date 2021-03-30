c571f79361ecd2ccbab209e4e819d75710702943
Bart Van Bos <bartvanbos@gmail.com>
2021-03-30 18:59:38 +0200

    added mtls scenarios

--- scenarios/run_all_find_concurreny_max_qps.sh
+++ scenarios/run_all_find_concurreny_max_qps.sh
@@ -15,6 +15,7 @@ SCENARIO_ARRAY=(
   # "3pod-diffnode-nosidecar-1core"
   # "3pod-diffnode-nosidecar-maxcore"
   # "3pod-samenode-nosidecar-1core"
+  
   # "1pod-diffnode-sidecar-1core-default"
   # "1pod-diffnode-sidecar-2core-default"
   # "1pod-diffnode-sidecar-3core-default"
@@ -25,10 +26,25 @@ SCENARIO_ARRAY=(
   # "1pod-samenode-sidecar-maxcore-default"
   # "2pod-diffnode-sidecar-1core-default"
   # "2pod-diffnode-sidecar-maxcore-default"
-  "2pod-samenode-sidecar-1core-default"
-  "3pod-diffnode-sidecar-1core-default"
-  "3pod-diffnode-sidecar-maxcore-default"
-  "3pod-samenode-sidecar-1core-default"  
+  # "2pod-samenode-sidecar-1core-default"
+  # "3pod-diffnode-sidecar-1core-default"
+  # "3pod-diffnode-sidecar-maxcore-default"
+  # "3pod-samenode-sidecar-1core-default" 
+  
+  "1pod-diffnode-sidecar-mtls-1core-default"
+  "1pod-diffnode-sidecar-mtls-2core-default"
+  "1pod-diffnode-sidecar-mtls-3core-default"
+  "1pod-diffnode-sidecar-mtls-maxcore-default"
+  "1pod-samenode-sidecar-mtls-1core-default"
+  "1pod-samenode-sidecar-mtls-2core-default"
+  "1pod-samenode-sidecar-mtls-3core-default"
+  "1pod-samenode-sidecar-mtls-maxcore-default"
+  "2pod-diffnode-sidecar-mtls-1core-default"
+  "2pod-diffnode-sidecar-mtls-maxcore-default"
+  "2pod-samenode-sidecar-mtls-1core-default"
+  "3pod-diffnode-sidecar-mtls-1core-default"
+  "3pod-diffnode-sidecar-mtls-maxcore-default"
+  "3pod-samenode-sidecar-mtls-1core-default"  
 )
 
 for SCENARIO in "${SCENARIO_ARRAY[@]}" ; do

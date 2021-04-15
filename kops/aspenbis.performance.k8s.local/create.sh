1e650074883b3218ceb04157d7bcebe8d62f4e68
Bart Van Bos <bartvanbos@gmail.com>
2021-04-15 12:28:00 +0200

    added second cluster for parallel testing

---
+++ kops/aspenbis.performance.k8s.local/create.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# aws s3api create-bucket --bucket aspenmesh-performance-state-store --region eu-west-3 --create-bucket-configuration LocationConstraint=eu-west-3
+# aws s3api put-bucket-versioning --bucket aspenmesh-performance-state-store  --versioning-configuration Status=Enabled
+
+export NAME=aspenbis.performance.k8s.local
+export KOPS_STATE_STORE=s3://aspenmesh-performance-state-store
+
+kops replace --force -f *.yaml
+kops update cluster --yes --admin=87600h
+kops rolling-update cluster

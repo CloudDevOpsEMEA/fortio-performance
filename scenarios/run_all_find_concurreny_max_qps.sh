68ae796fe1833044e85b2b3214e872d13c71c462
Bart Van Bos <bartvanbos@gmail.com>
2021-04-21 13:16:15 +0200

    new mem and cpu tests

--- scenarios/run_all_find_concurreny_max_qps.sh
+++ scenarios/run_all_find_concurreny_max_qps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SCENARIO_ARRAY=(
-  "scenario_sidecars_no_mtls_scale_1"
+  "1pod-diffnode-nosidecar-1core-default"
   # "1pod-diffnode-sidecar-1core-default"
   # "1pod-diffnode-sidecar-1core-4conc-tuned"
   # "1pod-diffnode-sidecar-1core-6conc-tuned"
